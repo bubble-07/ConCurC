9129800c481965c2dc4830e23dbf05e2f3ab5ccd
Alexander Grabanski <spagrab@gmail.com>
2012-12-10 13:37:57 -0600

    Replacing main.c with a stub of a "test"
    Just because.

--- main.c
+++ main.c
@@ -1,74 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define WORDSIZE 4
+#include "dynarray.h"
+#include "memoryman.h"
 
-#define nothing 0
-#define expression 1
-#define integer 2
-#define function 3
-#define curlist 4
-typedef char type;
-typedef struct object {
-	type objtype;
-	void* objaddr;
-} object;
+DEFINE_DYNARRAY(char)
 
-const object globalnothing = {nothing, NULL};
-
-void* fromheap;
-int fromheapsize;
-void* toheap;
-int toheapsize;
-
-typedef struct list {
-	object car;
-	object cdr;
-} list;
-void raise(char* exception, object conflict) {
-	printf(exception);
-	int x = 1 / 0;
-	return;
+char doubleit(char in) {
+    return in * 2;
 }
-object constructexpression(object car, object cdr) {
-	object ret;
-	ret.objtype = expression;
-	ret.objaddr = malloc(sizeof(list));
-	list* temppointer = ret.objaddr;
-	temppointer->car = car;
-	temppointer->cdr = cdr;
-	return ret;
-}
-list getlist(object in) {
-	list temp;
-	if ((in.objtype == expression) | (in.objtype == curlist)) {
-		list temp = *((list*)in.objaddr);
-		return temp;
-	}
-	raise("This object is not a list, but you tried to get a list out of it", in);
-	return temp;
-}
-typedef list (*functionptr)(list);
-object constructint(int value) {
-	//int is an "unboxed" type. the "objaddr" holds the value directly
-	object temp;
-	temp.objtype = integer;
-	temp.objaddr = (void*) value;
-	return temp;
-}
-int addstrap(list toadd) {
-	return (int)toadd.car.objaddr + addstrap(getlist(toadd.cdr));
-}
-							
-void printint(object toprint) {
-	if (toprint.objtype == integer) {
-		printf("%i", toprint.objaddr);
-	}
-	return;
-}
+    
 
 int main (int argc, const char * argv[]) {
-	//Start the heap off with a decent-sized chunk of memory
-    printint(constructint(5));
-    return 0;
+    char_dynarray test = char_dynarray_fromarray((char[]){1, 2, 3, 4, 5, 6}, 6);
+    char_dynarray second = char_dynarray_copy(test);
+    test = char_dynarray_cat(test, second);
+    test = char_dynarray_map(test, &doubleit); 
+    free(second.begin);
+    return test.begin[5];
 }

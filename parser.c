5b863e24d3b45b34ba50e79d9c9425b76eef54b8
Alexander Grabanski <spagrab@gmail.com>
2012-12-23 17:53:58 -0600

    Oops... Forgot to commit some changes on previous.

--- parser.c
+++ parser.c
@@ -1,28 +1,52 @@
 #include "parser.h"
-lexid display(lexid in, lexid_tree_dynarray other) {
-    if (in.tokenval == INT) {
-        printf("%s", "integer: ");
-        printf("%d", in.attr.intval);
-    }
-    else if (in.tokenval == FLOAT) {
-        printf("%s", "float: ");
-        printf("%.20f", in.attr.floatval);
-    }
-    else {
-        printf("%s", "identifier: ");
-        printf("%d", in.tokenval);
-    }
-    printf("%s", "\n");
-    return in;
+
+parser_state parser_state_init(lexid_dynarray prog, size_t i) {
+    parser_state result;
+    result.program = prog;
+    result.index = i;
+    return result;
 }
 
-int main(int argc, const char * argv[]) {
-    parse_result parseresult = parse(lex());
-    lexid_tree AST = parseresult.AST;
-    lexid_tree_dfmap(AST, &display);    
-    return 0;
+lexid parser_state_getRel(parser_state in, size_t offset) {
+    return in.program.begin[in.index + offset];
 }
-                
-                
 
 
+parser_state consume(lexid toconsume, parser_state in) {
+    if (lexid_eq(toconsume, in.program.begin[in.index])) {
+       return parser_state_init(in.program, in.index + 1);
+    }
+    return in; //TODO: throw an error!
+}
+
+parse_part parse_list(parser_state state) {
+     parse_part result;
+     result.tree = lexid_tree_init(EXPR_LEXID);
+     state = consume(LPAREN_LEXID, state);
+     lexid current;
+     parse_part subexpr;
+     size_t i = state.index;
+     while (i < state.program.size && state.program.begin[i].tokenval != RPAREN) {
+         current = state.program.begin[i];
+         switch (current.tokenval) {
+             case LPAREN:
+                subexpr = parse_list(parser_state_init(state.program, i));
+                result.tree = lexid_tree_addchild(result.tree, subexpr.tree);
+                i = subexpr.state.index;
+                break;
+             default:
+                result.tree = lexid_tree_addchild(result.tree, lexid_tree_init(current));
+                break;
+         }
+         i++;
+      }
+      result.state.index = i;
+      return result;
+}
+
+parse_result parse(lex_result in) {
+     parse_result result;
+     result.backsymtable = in.backsymtable;
+     result.AST = parse_list(parser_state_init(in.program, 0)).tree;
+     return result;
+}

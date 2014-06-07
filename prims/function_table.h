364d1c7ebea50d047ee91b6cb4cc8178ba478d12
bubble-07 <spagrab@gmail.com>
2014-06-07 16:27:45 -0500

    Function_table now uses polymorph_ptrs
    (instead of using raw polymorphs. Should result in smaller
    function_table structures and cleaner, less unsafe code)

--- prims/function_table.h
+++ prims/function_table.h
@@ -9,49 +9,44 @@
 #ifndef FUNCTIONTABLE_DEFINED
 #define FUNCTIONTABLE_DEFINED
 
-DEFINE_DICT(lexid, polymorph)
+DEFINE_DICT(lexid, polymorph_ptr)
 //Define something that lets us get all stored polymorphs
-DEFINE_GET_ALL_VALS(lexid, polymorph)
-DEFINE_GET_PTR(lexid, polymorph)
-DEFINE_GET_ALL_VAL_PTRS(lexid, polymorph)
+DEFINE_GET_ALL_VALS(lexid, polymorph_ptr)
+DEFINE_GET_PTR(lexid, polymorph_ptr)
 
 
 typedef struct {
-    lexid_polymorph_dict dict;
+    lexid_polymorph_ptr_dict dict;
 } function_table;
 
 static function_table init_function_table() {
     function_table result;
-    result.dict = lexid_polymorph_dict_init(50);
+    result.dict = lexid_polymorph_ptr_dict_init(50);
     return result;
 }
 //Add an empty polymorph with the given name
 //If it already exists, do nothing
 static function_table add_empty_polymorph(function_table in, lexid s) {
-    in.dict = lexid_polymorph_dict_addNoDup(in.dict,
-                                    lexid_polymorph_bucket_make(s, make_empty_polymorph()));
+    in.dict = lexid_polymorph_ptr_dict_addNoDup(in.dict,
+                                    lexid_polymorph_ptr_bucket_make(s, make_empty_polymorph_ptr()));
     return in;
 }
 
-//Returns a dynamic array of all stored polymorphs
-static polymorph_dynarray get_all_polymorphs(function_table in) {
-    return lexid_polymorph_dict_get_all_values(in.dict);
-}
-//Returns a dynamic array of pointers to all store polymorphs
+//Returns a dynamic array of pointers to all stored polymorphs
 static polymorph_ptr_dynarray get_all_polymorph_ptrs(function_table in) {
-    return lexid_polymorph_dict_get_all_val_ptrs(in.dict);
+    return lexid_polymorph_ptr_dict_get_all_values(in.dict);
 }
 
 
 //Returns 1 if a given function name has already been registered
 static int has_polymorph(function_table in, lexid s) {
-    return lexid_polymorph_dict_exists(in.dict, s);
+    return lexid_polymorph_ptr_dict_exists(in.dict, s);
 }
 
 //Adds a function pointer to the polymorph with the given name (assumes it exists)
 static function_table add_function_ptr(function_table in, lexid name, function_ptr f) {
     //Get the polymorph corresponding to the name
-    polymorph* target = lexid_polymorph_dict_getptr(in.dict, name);
+    polymorph* target = lexid_polymorph_ptr_dict_get(in.dict, name);
     //Add the given function to the polymorph (which is still within the dict)
     *target = add_to_polymorph(*target, f);
     //Return the function table we modified.
@@ -70,13 +65,12 @@ static function_table add_function(function_table in, lexid name, function f) {
 
 //Gets the polymorph corresponding to a given name
 static polymorph get_polymorph(function_table in, lexid name) {
-    return lexid_polymorph_dict_get(in.dict, name);
+    return *lexid_polymorph_ptr_dict_get(in.dict, name);
 }
 
 //Gets us a pointer to the polymorph with the given name (assumes it exists)
-//NOTE: this is inherently UNSAFE if the pointed-to polymorph moves.
 static polymorph_ptr get_polymorph_ptr(function_table in, lexid name) {
-    return lexid_polymorph_dict_getptr(in.dict, name);
+    return lexid_polymorph_ptr_dict_get(in.dict, name);
 }
 
    
--- prims/polymorph.h
+++ prims/polymorph.h
@@ -7,8 +7,6 @@
 struct polymorph {
     function_ptr_dynarray options; //List of functions that may work
     TypeInfo type; //most general type that can be returned
-    int isNull; //integer that is set to 1 if the given polymorph is actually null
-                //this is used for expressing the results of lookup failures
 };
 typedef struct polymorph polymorph;
 
@@ -17,11 +15,18 @@ typedef polymorph* polymorph_ptr;
 DEFINE_DYNARRAY(polymorph)
 DEFINE_DYNARRAY(polymorph_ptr)
 
+//Creates a polymorph with no function options
 static polymorph make_empty_polymorph() {
     polymorph result;
     result.options = function_ptr_dynarray_make(1);
     result.type = make_empty_type();
-    result.isNull = 0;
+    return result;
+}
+
+//Same, but allocates on heap
+static polymorph_ptr make_empty_polymorph_ptr() {
+    polymorph_ptr result = memalloc(sizeof(polymorph));
+    *result = make_empty_polymorph();
     return result;
 }
 
@@ -58,14 +63,13 @@ static void print_polymorph_ptr(polymorph_ptr in, string_dynarray backsymtable) 
 }
 
 
-static polymorph polymorph_lookup_failure = {{0,0,NULL}, {{0,0,NULL}}, 1};
+static polymorph_ptr polymorph_ptr_lookup_failure = NULL;
 
-static int polymorph_eq(polymorph a, polymorph b) {
-    if (a.isNull && b.isNull) {
-        return 1; //Doing this so we can tell a lookup failure when we see one
+static int polymorph_ptr_eq(polymorph_ptr a, polymorph_ptr b) {
+    //Equal iff pointers equal
+    if (a == b) {
+        return 1;
     }
-    //I can't think of a reason we would test these for equality.
-    //However, we will leave this blank so we can define dicts on it
     return 0; //nope! not equal
 }
 

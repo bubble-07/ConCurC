8484f85c7d9651ca9ccd93434d0aed29e602a39c
bubble-07 <spagrab@gmail.com>
2014-06-22 17:31:02 -0500

    Changed equation_set to be simpler
    Equation_set now is simply a dynarray of equations in disguise, since we
    no longer store LH type_refs in equations.

--- prims/type_ref_info.h
+++ prims/type_ref_info.h
@@ -3,12 +3,12 @@
 #ifndef TYPE_REF_INFO_DEFINED
 #define TYPE_REF_INFO_DEFINED
 
-#include "type_equation.h"
+#include "equation_set.h"
 
 //The actual information is stored in a pointer from the "representative" node
 //Using the following structure
 typedef struct {
     TypeInfo upperbound;
-    type_equation_dynarray equations;
+    equation_set equations;
 } type_ref_info;
 #endif
